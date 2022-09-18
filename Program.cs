97614e466726bb7b1ae6a63709ac5d1e4c4aa135
Pavel <m83819@gmail.com>
2022-09-18 17:50:49 +0700

    added string array print method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача: написать программу, которая из имеющегося массива строк фомирует массив из строк, 
+длина которых меньше либо равна 3 символам.Первоначальный массив можгл ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.*/
+
+
+//ввод набора чисел с консоли строкой
+string[] ParseArray(string message)
+{
+    Console.WriteLine(message);
+    string[] nums = Console.ReadLine().Split(new char[] { ',' });
+    string[] strArray = new string[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        strArray[i] = nums[i];
+    }
+    return strArray;
+
+}
+
+
+
+
+//ввод значения с клавиатуры
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue=Console.ReadLine();
+    return int.Parse(readValue);
+}
+
+
+//распечатка массива
+void PrintArray (string[] col)
+{
+
+    int count=col.Length;
+    int position=0;
+
+    Console.Write("[ ");
+    while(position<count) 
+    {
+        if(position==count-1)  {Console.Write($" {col[position]}");}
+        else {Console.Write($"{col[position]}, ");}
+        
+        position++;
+    }
+    Console.WriteLine("] ");
+
+}
+
+string[] input=ParseArray("ВВедите массив строк через запятую");
+PrintArray(input);
